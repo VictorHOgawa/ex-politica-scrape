1a01f697e88437a1c5fa86a7e2862f223a62b3f3
VictorOgawa <victor.ogawa@gmail.com>
2023-11-10 14:16:02 -0400

    test dripcrawler

--- Spiders/spiders/test.py
+++ Spiders/spiders/test.py
@@ -1,7 +1,17 @@
 import requests
 
-s = requests.Session()
-r = s.get('https://www.folhamax.com/includes/__lista_noticias.inc.php?pageNum_Pagina=0&query_string=/politica/&totalRows_Pagina=69728',verify=False)
-r = s.post('https://www.folhamax.com/includes/__lista_noticias.inc.php?pageNum_Pagina=0&query_string=/politica/&totalRows_Pagina=69728', auth=('user', 'pass'),verify=False)
-print(r.status_code)
-print(r.text)
+url = "https://dripcrawler.p.rapidapi.com/"
+
+payload = {
+	"url": "https://www.folhamax.com/includes/__lista_noticias.inc.php?pageNum_Pagina=0&query_string=/politica/&totalRows_Pagina=69728",
+	"javascript_rendering": "False"
+}
+headers = {
+	"content-type": "application/json",
+	"X-RapidAPI-Key": "941a69a26dmshfef948a2824884cp1b23eajsn68e4d5b09e8b",
+	"X-RapidAPI-Host": "dripcrawler.p.rapidapi.com"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.json())
