fe9ced512755ba3fa55e95ad92c3463a73dd22c6
VictorOgawa <victor.ogawa@gmail.com>
2023-11-14 09:25:54 -0400

    change path

--- Apify/Facebook/Facebook_Pages.py
+++ Apify/Facebook/Facebook_Pages.py
@@ -69,7 +69,7 @@ for item in client.dataset(run["defaultDatasetId"]).iterate_items():
     
     json_str = json.dumps(json_array, indent=4, ensure_ascii=False)
     
-with open(f"/home/scrapeops/Axioon/Apify/Results/Facebook_Page.json", "w") as f:
+with open(f"/home/scrapeops/Axioon/Apify/Facebook_Page.json", "w") as f:
     f.write(json_str)
     
-upload_file(f"/home/scrapeops/Axioon/Apify/Results/Facebook_Page.json", "nightapp", f"MT/Apify/Facebook/Facebook_Page_{timestamp}.json")
+upload_file(f"/home/scrapeops/Axioon/Apify/Facebook_Page.json", "nightapp", f"MT/Apify/Facebook/Facebook_Page_{timestamp}.json")
