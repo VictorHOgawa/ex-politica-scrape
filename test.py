ae8da4a69220ed9ad858302ba4b28d725ba910f0
VictorOgawa <victor.ogawa@gmail.com>
2023-11-14 16:24:09 -0400

    try for real

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # from selenium import webdriver
 # from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.common.by import By
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 # from time import sleep
 # import re
 # from selenium.webdriver.chrome.options import Options as ChromeOptions
@@ -35,6 +35,14 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
  
-driver.get("https://python.org")
-print(driver.title)
-driver.close()
+driver.get("https://www.jornalopcao.com.br/categoria/politica/?pg=1")
+
+page_source = driver.page_source
+
+soup = BeautifulSoup(page_source)
+
+article = soup.find("div", class_="template-part-component-feed-item")
+
+article_link = article.find("a", class_="main_link").get("href")
+
+print("article_link: ", article_link)
