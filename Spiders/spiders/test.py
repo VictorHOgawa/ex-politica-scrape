383517f7ed87813e16c95cda4cc377b04b622254
VictorOgawa <victor.ogawa@gmail.com>
2023-11-17 13:23:51 -0400

    test scrape date

--- Spiders/spiders/test.py
+++ Spiders/spiders/test.py
@@ -2,29 +2,65 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-url = "https://dripcrawler.p.rapidapi.com/"
+start_url_extension = "/includes/__lista_noticias.inc.php?pageNum_Pagina=0&query_string=/politica/&totalRows_Pagina=69728"
+next_page = None
 
-payload = {
-	"url": "https://www.folhamax.com/includes/__lista_noticias.inc.php?pageNum_Pagina=0&query_string=/politica/&totalRows_Pagina=69728",
-	"javascript_rendering": "False"
-}
-headers = {
-	"content-type": "application/json",
-	"X-RapidAPI-Key": "941a69a26dmshfef948a2824884cp1b23eajsn68e4d5b09e8b",
-	"X-RapidAPI-Host": "dripcrawler.p.rapidapi.com"
-}
+while True:
+	url = "https://dripcrawler.p.rapidapi.com/"
 
-response = requests.post(url, json=payload, headers=headers)
+	main_url = "https://www.folhamax.com"
 
-html = response.json()['extracted_html']
+	if next_page is not None:
+		start_url_extension = next_page
+ 
+	payload_url = main_url + start_url_extension
 
-bs = BeautifulSoup(html, 'html.parser')
+	payload = {
+		"url": f"{payload_url}",
+		"javascript_rendering": "False"
+	}
+	headers = {
+		"content-type": "application/json",
+		"X-RapidAPI-Key": "941a69a26dmshfef948a2824884cp1b23eajsn68e4d5b09e8b",
+		"X-RapidAPI-Host": "dripcrawler.p.rapidapi.com"
+	}
 
-article_banner = bs.find_all("a", {"class": "w-100"})
+	response = requests.post(url, json=payload, headers=headers)
 
-links = []
+	html = response.json()['extracted_html']
 
-for link in article_banner:
-	links.append(link['href'])
+	bs = BeautifulSoup(html, 'html.parser')
 
-print(links)
+	article_banner = bs.find_all("a", {"class": "w-100"})
+
+	links = []
+
+	for link in article_banner:
+		links.append(link['href'])
+		url = "https://dripcrawler.p.rapidapi.com/"
+
+		article_payload_url = link["href"]
+
+		payload = {
+		"url": f"{article_payload_url}",
+		"javascript_rendering": "False"
+		}
+		headers = {
+			"content-type": "application/json",
+			"X-RapidAPI-Key": "941a69a26dmshfef948a2824884cp1b23eajsn68e4d5b09e8b",
+			"X-RapidAPI-Host": "dripcrawler.p.rapidapi.com"
+		}
+
+		article_response = requests.post(url, json=payload, headers=headers)
+
+		article_html = article_response.json()['extracted_html']
+
+		article_bs = BeautifulSoup(article_html, 'html.parser')
+
+		article_updated = article_bs.find("span").text
+  
+		print("article_updated: ", article_updated)
+
+	next_page = bs.find("a", {"class": "next"}).get("href")
+ 
+	start_url_extension = next_page
