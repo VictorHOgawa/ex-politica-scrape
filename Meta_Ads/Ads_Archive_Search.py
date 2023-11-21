5e02a6eee6b1c2862423ed58124327f73b720686
VictorOgawa <victor.ogawa@gmail.com>
2023-11-21 17:03:57 -0400

    try to fix meta ads

--- Meta_Ads/Ads_Archive_Search.py
+++ Meta_Ads/Ads_Archive_Search.py
@@ -33,12 +33,17 @@ now = datetime.now()
 now_in_days = now.strftime("%Y-%m-%d")
 timestamp = datetime.timestamp(now)
 
+input = requests.get("http://172.20.10.2:3333/scrape/name")
+input = input.json()
+
+input = input["list"]
+
 search_amount = [
-    {"name": "Mauro Mendes Ferreira", "delivery_by_region": "Mato Grosso", "bylines": "ELEICAO 2022 MAURO MENDES FERREIRA GOVERNADOR, Mauro Mendes Ferreira", "ad_delivery_date_min": "2023-09-01"}, 
-    # {"name": "Roberto Dorner", "delivery_by_region": "Mato Grosso", "bylines": "ELEICAO 2020 ROBERTO DORNER PREFEITO - CNPJ DO CANDIDATO 38.551.189/0001-83, Roberto Dorner", "ad_delivery_date_min": "2023-09-01"},
+    # {"name": f"{name["name"]}", "bylines": f"{name["name"]}, {name["facebook"]}", "ad_delivery_date_min": "2022-09-01"} for name in input
+    {"name": "Roberto Naves", "bylines": "Roberto Naves, Roberto Naves Prefeito", "ad_delivery_date_min": "2022-09-01"}
 ]
 
-search_queries = {"ad_reached_countries": "BR", "search_terms": "", "delivery_by_region": "", "ad_delivery_date_min": "", "bylines": "", "ad_type": "POLITICAL_AND_ISSUE_ADS", "fields": "ad_creation_time,ad_delivery_start_time,ad_delivery_stop_time,ad_snapshot_url,bylines,page_name,currency,spend,impressions,delivery_by_region,demographic_distribution","limit": 5000, "access_token": "EAACxJFtlwx0BOzhZB2ncAYDjgwtgRdVwzkVVUD9ZAE4v46EhwBgajFChBeZBDMpmYeUg7mkAD6G5UC6ovaynnpW5GSDRz8pN7Mecu7uKnZB4RkBrS8Evuj3MSpwZCJZAZAXtsrOjw1sFOHjAZCYYorHDeFsZB7mrBcXucQFsUHzpodBTm7igBohdzSHk7ubIx4mZB6l4kSUOsuJOEqZBNYscitK"}
+search_queries = {"ad_reached_countries": "BR", "search_terms": "", "ad_delivery_date_min": "", "bylines": "", "ad_type": "POLITICAL_AND_ISSUE_ADS", "fields": "ad_creation_time,ad_delivery_start_time,ad_delivery_stop_time,ad_snapshot_url,bylines,page_name,currency,spend,impressions,delivery_by_region,demographic_distribution","limit": 5000, "access_token": "EAACxJFtlwx0BOzhZB2ncAYDjgwtgRdVwzkVVUD9ZAE4v46EhwBgajFChBeZBDMpmYeUg7mkAD6G5UC6ovaynnpW5GSDRz8pN7Mecu7uKnZB4RkBrS8Evuj3MSpwZCJZAZAXtsrOjw1sFOHjAZCYYorHDeFsZB7mrBcXucQFsUHzpodBTm7igBohdzSHk7ubIx4mZB6l4kSUOsuJOEqZBNYscitK"}
 
 current_version = "v18.0"
 
@@ -46,16 +51,12 @@ search_url = f"https://graph.facebook.com/{current_version}/ads_archive?"
 
 for item in search_amount:
     item["name"] = item["name"].replace(" ", "%20")
-    item["delivery_by_region"] = item["delivery_by_region"].replace(" ", "%20")
     item["bylines"] = item["bylines"].replace(" ", "%20")
     search_queries["search_terms"] = item["name"]
-    search_queries["delivery_by_region"] = item["delivery_by_region"]
     search_queries["bylines"] = item["bylines"]
     search_queries["ad_delivery_date_min"] = item["ad_delivery_date_min"]
 
-    r = requests.get(f"{search_url}ad_reached_countries={search_queries['ad_reached_countries']}&search_terms={search_queries['search_terms']}&delivery_by_region={search_queries['delivery_by_region']}&ad_delivery_date_min={search_queries['ad_delivery_date_min']}&bylines={search_queries['bylines']}&ad_type={search_queries['ad_type']}&fields={search_queries['fields']}&limit={search_queries['limit']}&access_token={search_queries['access_token']}")
-
-    print("r: ", r.content)
+    r = requests.get(f"{search_url}ad_reached_countries={search_queries['ad_reached_countries']}&search_terms={search_queries['search_terms']}&ad_delivery_date_min={search_queries['ad_delivery_date_min']}&bylines={search_queries['bylines']}&ad_type={search_queries['ad_type']}&fields={search_queries['fields']}&limit={search_queries['limit']}&access_token={search_queries['access_token']}")
 
     file_name = item["name"].replace("%20","-")
     
@@ -65,7 +66,10 @@ for item in search_amount:
     
     json_str = json.dumps(json_str, ensure_ascii=False, indent=4)
 
-    with open(f"{file_name}.json", "w") as f:
+    with open("test_Roberto_Naves_e_Siqueira.json", "w") as f:
         f.write(json_str)
 
-    upload_file(f"{file_name}.json", "nightapp", f"MT/Meta_Ads/{file_name}_{timestamp}.json")
+    # with open(f"{file_name}.json", "w") as f:
+    #     f.write(json_str)
+
+    # upload_file(f"{file_name}.json", "nightapp", f"MT/Meta_Ads/{file_name}_{timestamp}.json")
