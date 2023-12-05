f7012fc529b7946c7f8faf278213ad58a210a697
VictorOgawa <victor.ogawa@gmail.com>
2023-12-05 12:47:40 -0400

    commit

--- Separate_Spiders/Mt_MidiaNews_CF.py
+++ Separate_Spiders/Mt_MidiaNews_CF.py
@@ -125,6 +125,8 @@ while True:
 		article_updated = article_updated.replace(".", "/")
 		article_updated = article_updated.strip()
 		article_updated = datetime.strptime(article_updated, "%d/%m/%Y")
+  
+		print("updated: ", article_updated)
 		
 		##
 		## article_title
