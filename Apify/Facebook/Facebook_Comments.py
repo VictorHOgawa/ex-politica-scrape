6240bb3c3b206905340b2191032c91c6471f8511
VictorHOgawa <victor.ogawa@gmail.com>
2024-01-11 14:59:58 -0400

    re-add upload_file function

--- Apify/Facebook/Facebook_Comments.py
+++ Apify/Facebook/Facebook_Comments.py
@@ -1,10 +1,37 @@
-from ...upload_file import upload_file
+from botocore.exceptions import ClientError
 from apify_client import ApifyClient
 from dotenv import load_dotenv
 from datetime import datetime
 import requests
+import logging
+import boto3
 import json
 import os
+
+load_dotenv()
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 "bucket"
+
+    :param file_name: File to upload
+    :param "bucket": "bucket" to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3', aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+        acl = s3_client.put_object_acl(Bucket=bucket, Key=object_name, ACL='public-read')
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 load_dotenv()
 
--- Meta_Ads/Ads_Archive_Search.py
+++ Meta_Ads/Ads_Archive_Search.py
@@ -1,12 +1,36 @@
 from datetime import date, datetime, timedelta
-from ..upload_file import upload_file
+from botocore.exceptions import ClientError
 from dotenv import load_dotenv
 import requests
+import logging
+import boto3
 import json
 import os
 
 load_dotenv()
 
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 "bucket"
+
+    :param file_name: File to upload
+    :param "bucket": "bucket" to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3', aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+        acl = s3_client.put_object_acl(Bucket=bucket, Key=object_name, ACL='public-read')
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 now = datetime.now()
 now_in_days = now.strftime("%Y-%m-%d")
 timestamp = datetime.timestamp(now)
